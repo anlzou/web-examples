44b600eda105a988d32bfb31a17aa2f74fb260e6
anlzou <599502931@qq.com>
2020-04-01 23:29:05 +0800

    练习(test.html、test.js)：JavaScript 事件练习

---
+++ js/test.js
@@ -0,0 +1,13 @@
+/*
+ * @Date        : 2020-04-01 21:48:05
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-04-01 21:58:38
+ * @FilePath    : \html\js\test.js
+ * @Describe    :
+ */
+
+//document.getElementById("text").style.fontSize = "12px";
+function key() {
+    alert("hello anlzou.");
+}
