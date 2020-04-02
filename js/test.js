e105ef10894f0b4f84c6cd24d4a7c303a1e362b7
anlzou <599502931@qq.com>
2020-04-02 23:59:09 +0800

    学习(test.html、test.js)：三目运算学习

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@
  * @Date        : 2020-04-01 21:48:05
  * @LastEditors : anlzou
  * @Github      : https://github.com/anlzou
- * @LastEditTime: 2020-04-01 21:58:38
+ * @LastEditTime: 2020-04-02 23:06:47
  * @FilePath    : \html\js\test.js
  * @Describe    :
  */
@@ -10,4 +10,10 @@
 //document.getElementById("text").style.fontSize = "12px";
 function key() {
     alert("hello anlzou.");
+    //art();
+}
+function art() {
+    var a = 6;
+    // 结果"a不是1,2,3"
+    1 == a ? console.log("a是1") : 2 == a ? console.log("a是2") : 3 == a ? console.log("a是3") : console.log("a不是1,2,3");
 }
