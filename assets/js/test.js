2ad7aedaba9bb3d59a086391d6a915605f929cec
anlzou <599502931@qq.com>
2020-04-23 17:52:31 +0800

    test(HTML DOM):动态添加/删除节点

--- assets/js/test.js
+++ assets/js/test.js
@@ -2,8 +2,8 @@
  * @Date        : 2020-04-01 21:48:05
  * @LastEditors : anlzou
  * @Github      : https://github.com/anlzou
- * @LastEditTime: 2020-04-02 23:06:47
- * @FilePath    : \html\js\test.js
+ * @LastEditTime: 2020-04-23 17:49:56
+ * @FilePath    : \html\assets\js\test.js
  * @Describe    :
  */
 
@@ -16,4 +16,25 @@ function art() {
     var a = 6;
     // 结果"a不是1,2,3"
     1 == a ? console.log("a是1") : 2 == a ? console.log("a是2") : 3 == a ? console.log("a是3") : console.log("a不是1,2,3");
+}
+
+
+window.onload = function () {
+    var button_add = document.getElementById("btn_add");
+    var button_del = document.getElementById("btn_del");
+
+    button_add.onclick = function click() {
+        var para = document.createElement("p");
+        var node = document.createTextNode("这是一个新段落。");
+        para.appendChild(node);
+
+        var element = document.getElementById("div1");
+        var child = document.getElementById("p1");
+        element.insertBefore(para, child);
+    }
+    button_del.onclick = function click() {
+        var element = document.getElementById("div1");
+        var node = document.getElementsByTagName("p");
+        element.removeChild(node[0]);
+    }
 }
