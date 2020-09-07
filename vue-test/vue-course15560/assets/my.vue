8dca481c45a520ffa586a5cad232ebc2874aa2c7
anlzou <599502931@qq.com>
2020-09-07 22:57:14 +0800

    add(vue-course15560/):自定义组件练习

---
+++ vue-test/vue-course15560/assets/my.vue
@@ -0,0 +1,29 @@
+<!--
+ * @Date        : 2020-09-07 21:51:45
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-09-07 21:57:17
+ * @FilePath    : \web-test\vue-test\vue-course15560\assets\my.vue
+ * @Describe    : 
+-->
+<template>
+  <div id="app">
+    <p v-bind:class="{active:isActive}">我是p标签</p>
+  </div>
+</template>
+
+<script>
+var app = new Vue({
+  el: "#app",
+  data: {
+    ifActive: true,
+  },
+});
+export default {};
+</script>
+
+<style>
+.active {
+  background-color: aqua;
+}
+</style>
