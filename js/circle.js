b1451c21e200a04bf6ca11fe2dc975ce209f6a66
anlzou <599502931@qq.com>
2020-09-08 22:51:37 +0800

    export&import test

---
+++ js/circle.js
@@ -0,0 +1,18 @@
+export function area(radius) {
+  return Math.PI * radius * radius;
+}
+
+export function circumference(radius) {
+  return 2 * Math.PI * radius;
+// }
+
+// function area(radius) {
+//   return Math.PI * radius * radius;
+// }
+
+// function circumference(radius) {
+//   return 2 * Math.PI * radius;
+// }
+
+// console.log('圆面积：' + area(4));
+// console.log('圆周长：' + circumference(14));
---
+++ js/main.js
@@ -0,0 +1,5 @@
+import {area, circumference} from "circle.js";
+
+function main_area(){
+	console.log(area(4));
+}
