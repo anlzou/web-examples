629fbfce3a1aca245b73dc58dd6c707915d2a3aa
anlzou <599502931@qq.com>
2020-10-01 22:17:02 +0800

    update(webpackDemo):05

--- vue-test/webpack-course24292/webpack4.x讲义和代码/代码/05/src/module.js
+++ vue-test/webpack-course24292/webpack4.x讲义和代码/代码/05/src/module.js
@@ -1,3 +1,11 @@
+/*
+ * @Date        : 2020-07-10 20:13:34
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-10-01 22:12:39
+ * @FilePath    : \webpack-course24292\webpack4.x讲义和代码\代码\05\src\module.js
+ * @Describe    : 
+ */
 module.exports = {
-    name : 'Mr.Zhang'
+    name : 'Mr.An'
 };
