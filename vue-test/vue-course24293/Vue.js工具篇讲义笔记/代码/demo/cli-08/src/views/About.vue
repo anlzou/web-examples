56e628dfbd2b92d11c3da529c591ca960b2ab371
anlzou <599502931@qq.com>
2020-10-06 21:23:07 +0800

    add(cli-08/):finish end 24

---
+++ vue-test/vue-course24293/Vue.js工具篇讲义笔记/代码/demo/cli-08/src/views/About.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Date        : 2020-10-06 20:24:09
+ * @LastEditors : anlzou
+ * @Github      : https://github.com/anlzou
+ * @LastEditTime: 2020-10-06 20:30:15
+ * @FilePath    : \web-test\vue-test\vue-course24293\Vue.js工具篇讲义笔记\代码\demo\cli-08\src\views\About.vue
+ * @Describe    : 
+-->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+<style lang="less">
+@color: red;
+
+.about h1 {
+  color: @color;
+}
+</style>
