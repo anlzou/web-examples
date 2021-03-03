8016e7d33106d03e40974e227d9836e4dd9fe045
anlzou <599502931@qq.com>
2021-03-03 16:44:33 +0800

    update

--- node.js/server/README.md
+++ node.js/server/README.md
@@ -1,1 +1,3 @@
 paper:https://www.nodebeginner.org/index-zh-cn.html
+
+[module.exports 与 exports，export 与 export default 之间的关系和区别](https://www.cnblogs.com/fayin/p/6831071.html)
--- node.js/server/server.js
+++ node.js/server/server.js
@@ -1,13 +1,13 @@
 var http = require("http");
 var url = require("url");
 
-function start(route) {
+function start(route,handle) {
   function onRequest(request, resopne) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request from " + pathname + " received.");
 
-    route(pathname);
-    
+    route(handle,pathname);
+
     resopne.writeHead(200, { "Content-Type": "text/plain" });
     resopne.write("Hello, this is server.");
     resopne.end();
